23d6d27ce89ef7aec46b28b5dd8a85607b75c78d
Carlos Narez <cnarez@gmail.com>
2014-09-12 16:00:18 -0600

    adding protocol and domain methods

--- url-splitting/spec/UrlSplitterSpec.php
+++ url-splitting/spec/UrlSplitterSpec.php
@@ -12,11 +12,49 @@ class UrlSplitterSpec extends ObjectBehavior
 
     public function it_returns_an_array()
     {
-        $this->split('http://google.com')->shouldBeArray();
+        $url = 'http://google.com';
+        $this->split($url)->shouldBeArray();
     }
 
-    public function it_returns_protocol_domain_in_the_first_key()
+    public function it_gets_protocol_url()
     {
-        $this->split('http://google.com')->shouldHave(['protocol' => 'http']);
+        $url = 'http://google.com';
+        $this->protocol($url)->shouldBe('http');
+    }
+
+    public function it_gets_other_kind_of_protocol()
+    {
+        $url = 'ftp://google.com';
+        $this->protocol($url)->shouldBe('ftp');
+    }
+
+    public function it_gets_a_empty_protocol()
+    {
+        $url = 'google.com';
+        $this->protocol($url)->shouldBe('');
+    }
+
+    public function it_gets_a_simple_domain()
+    {
+        $url = 'http://google.com';
+        $this->domain($url)->shouldBe('google.com');
+    }
+
+    public function it_gets_a_complex_domain()
+    {
+        $url = 'http://gt.docs.google-1.com.gt/search?q=car';
+        $this->domain($url)->shouldBe('gt.docs.google-1.com.gt');
+    }
+
+    public function it_raise_exception_with_invalid_domain()
+    {
+        $url = 'google';
+        $this->shouldThrow('InvalidArgumentException')->duringDomain($url);
+    }
+
+    public function it_gets_the_last_part_of_url()
+    {
+        $url = 'http://google.com/search?q=car';
+        $this->path($url)->shouldBe('search?q=car');
     }
 }
--- url-splitting/src/UrlSplitter.php
+++ url-splitting/src/UrlSplitter.php
@@ -3,10 +3,34 @@
 class UrlSplitter
 {
     public function split($url)
+    {              
+        return [
+            'protocol' => $this->protocol($url),
+            'domain' => $this->domain($url),
+            'path' => ''
+        ];
+    }
+
+    public function protocol($url)
     {
-        list($protocol, $other) = explode('://', $url);
-        $components ['protocol'] = $protocol;
-        var_dump($components);
-        return $components;
+        preg_match('/.+(?=:\/\/)/', $url, $matches);
+
+        return  (empty($matches)) ? '' : $matches[0];
+    }
+
+    public function domain($url)
+    {
+        preg_match('/(?<=:\/\/)[a-z][a-z|\.|0-9|-]+[a-z]{2,5}/', $url, $matches);
+
+        if(empty($matches)) throw new InvalidArgumentException();
+
+        return $matches[0];
+    }
+
+    public function path($url)
+    {
+        preg_match('/(?<=[a-z]\/).+/', $url, $matches);
+
+        return (empty($matches[0])) ? '' : $matches[0];
     }
 }
