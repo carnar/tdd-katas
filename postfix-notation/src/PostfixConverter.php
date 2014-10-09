3f6fbea0a54530a02ea136660d4cac69f90064b1
Carlos Narez <cnarez@gmail.com>
2014-10-09 12:31:36 -0600

    before big refactor in postfix converter

--- postfix-notation/src/PostfixConverter.php
+++ postfix-notation/src/PostfixConverter.php
@@ -3,44 +3,129 @@
 class PostfixConverter {
 
     protected $stack;
-    protected $operatorStack;
+    protected $precedence = [
+        '+' => 1,
+        '-' => 1,
+        '*' => 2,
+        '/' => 2
+    ];
 
-    function __construct()
+    function __construct(PostfixStack $stack)
     {
-        $this->stack = new PostfixStack;
-        $this->operatorStack = new PostfixStack;
+        $this->stack = $stack;
     }
 
     public function explode(&$expression)
     {
         while(strlen($expression) > 0)
         {
-            $component = $this->nextExpressionComponent($expression);
+            $component = $this->extractNextComponentExpression($expression);
 
-            if($size = strlen($component))
-            {
-                $expression = substr($expression, $size, strlen($expression));
-            }
-            else
-            {
-                $expression = substr($expression, 1, strlen($expression));   
-            }
+            $expression = $this->deleteComponentFromExpression($expression, $component);
+
             $this->explode($expression);    
         }
     }
 
-    public function nextExpressionComponent($expression)
+    public function deleteComponentFromExpression($expression, $component)
     {
-        if(preg_match('/^\d+/', $expression, $match))
+        return substr($expression, strlen($component), strlen($expression));   
+    }
+
+    public function extractNextComponentExpression($expression)
+    {
+        if(strlen($expression))
         {
-            $this->stack->push($match[0]);
-        }
-        elseif(preg_match('/^(\+|-|\*|\/)/', $expression, $match))
-        {
-            $this->operatorStack->push($match[0]);
+            if(preg_match('/^\d+/', $expression, $match))
+            {
+                // $this->stack->push($match[0]);
+                $this->applyNumberRules($match[0]);
+            }
+            elseif($match = $this->nextIsOperator($expression))
+            {
+                $this->stack->push($match[0]);
+            }            
+            else
+            {
+                $match[0] = $expression[0];
+            }
         }
 
         return isset($match[0]) ? $match[0] : false;
+    }
+
+    public function applyNumberRules($number)
+    {
+        if($this->areTheLastTwoOperators())
+        {
+            $this->resolveOperatorsPrecedence($number);
+        }
+        elseif($this->isTheLastAnOperator())
+        {
+            $last = $this->stack->pop();
+
+            $this->stack->push($number);
+            $this->stack->push($last);
+        }
+        else
+        {
+            $this->stack->push($number);
+        }
+    }
+
+    public function resolveOperatorsPrecedence($number)
+    {
+        $last = $this->stack->pop();
+        $previous = $this->stack->pop();
+
+        if($this->hasMorePrecedence($previous, $last))
+        {
+            $this->stack->push($number);
+            $this->stack->push($last);
+            $this->stack->push($previous);
+        }
+        else
+        {
+            $this->stack->push($previous);                        
+            $this->stack->push($number);
+            $this->stack->push($last);
+        }
+    }
+
+    public function isTheLastAnOperator()
+    {
+        if($this->stack->count() >= 1)
+        {
+            $last = $this->stack->pop();
+
+            $this->stack->push($last);
+
+            return ($this->nextIsOperator($last));
+        }
+
+        return false;
+    }
+
+    public function areTheLastTwoOperators()
+    {
+        if($this->stack->count() > 1)
+        {
+            $last = $this->stack->pop();
+            $previous = $this->stack->pop();
+
+            $this->stack->push($previous);
+            $this->stack->push($last);
+
+            return ($this->nextIsOperator($last) && $this->nextIsOperator($previous));
+        }
+        return false;
+    }
+
+    public function nextIsOperator($expression)
+    {
+        $result = preg_match('/^(\+|-|\*|\/)/', $expression, $match);
+
+        return (isset($match[0])) ? $match[0] : $result;
     }
 
     /**
@@ -51,21 +136,17 @@ class PostfixConverter {
      */
     public function convert($expression)
     {
-        $result = '';
-
         $this->explode($expression);
+        
+        return implode(' ', $this->stack->release());
+    }
 
-        if($this->stack->count())
-        {
-            $result = implode(' ', $this->stack->release());
-
-            if($this->operatorStack->count())
-            {
-                $result .= ' ' . implode(' ', $this->operatorStack->release()); 
-            }
-        }
-
-        return $result; 
+    public function hasMorePrecedence($base, $target)
+    {
+        if( ! array_key_exists($base, $this->precedence) || ! array_key_exists($target, $this->precedence))
+            return false;
+        
+        return ($this->precedence[$target] > $this->precedence[$base]);
     }
 
 }
--- postfix-notation/tests/PostfixConverterTest.php
+++ postfix-notation/tests/PostfixConverterTest.php
@@ -6,7 +6,7 @@ class PostfixConverterTest extends PHPUnit_Framework_TestCase {
 
     public function setUp()
     {
-        $this->converter = new PostfixConverter;
+        $this->converter = new PostfixConverter(new PostfixStack);
     }
 
     public function test_if_all_are_ok()
@@ -21,6 +21,13 @@ class PostfixConverterTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('5', $postfix);
     }
 
+    public function test_if_is_an_operator()
+    {
+        $expression = '+2';
+
+        $this->assertEquals('+', $this->converter->nextIsOperator($expression)[0]);
+    }
+
     public function test_convert_add_operation()
     {
         $postfix = $this->converter->convert('2+3');
@@ -28,7 +35,46 @@ class PostfixConverterTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('2 3 +', $postfix);
     }
 
+    public function test_convert_dual_operation()
+    {
+        $postfix = $this->converter->convert('2+3-1');
 
-    
+        $this->assertEquals('2 3 + 1 -', $postfix);
+    }
+
+    public function test_complex_add_and_substract_operation()
+    {
+        
+        $postfix = $this->converter->convert('2+3-1+20-2-2');
+
+        $this->assertEquals('2 3 + 1 - 20 + 2 - 2 -', $postfix);
+    }
+
+    public function test_operation_with_no_reorder_operators_require()
+    {
+        $postfix = $this->converter->convert('2*3+4');
+
+        $this->assertEquals('2 3 * 4 +', $postfix);        
+    }
+
+    public function test_operation_reordering_operators_for_precedence()
+    {
+        $postfix = $this->converter->convert('2+3*4');
+
+        $this->assertEquals('2 3 4 * +', $postfix);
+    }
+
+    public function test_eval_operator_precedence()
+    {
+        $base = '*';
+        $target = '+';
+
+        $result = $this->converter->hasMorePrecedence($base, $target);
+        $this->assertEquals(false, $result);
+
+        $result = $this->converter->hasMorePrecedence($target, $base);
+        $this->assertEquals(true, $result);
+    }
+
 }
 
